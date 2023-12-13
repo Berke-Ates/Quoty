e487e9321ac5435d4b16e58a5b3d3ef456965c42
Berke Ates <beates@student.ethz.ch>
2023-12-13 18:41:43 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
       <h1 id="q" class="animate__animated"></h1>
       <i><h2 id="a" class="animate__animated"></h2></i>
     </div>
+		<div class="overlay">
+				Click / Tap: Random Quote, Left/Right Arrow Keys: Prev/Next Quote
+		</div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.2.0/p5.min.js"></script>
 		<script type="text/javascript" src="quotes.json"></script>
--- main.js
+++ main.js
@@ -2,30 +2,30 @@ let q = "";
 let a = "";
 let ready = false;
 
-function queryQuote() {
-  let intId = id;
+function queryQuote(){
+	let intId = id;
 
-  if (id < 0) {
-    intId = Math.floor(Math.random() * quotes.length);
-  }
+	if(id < 0){
+		intId = Math.floor(Math.random() * quotes.length);
+	}
 
-  intId = Math.min(Math.max(intId, 0), quotes.length - 1);
+	intId = Math.min(Math.max(intId, 0), quotes.length-1);
 
-  let r = quotes[intId % quotes.length];
-  q = r["quote"];
-  if (r["author"] != "") {
-    a = r["author"];
-  } else {
-    a = "unknown";
-  }
-  var queryParams = new URLSearchParams(window.location.search);
-  queryParams.set("id", intId);
-  history.replaceState(null, null, "?" + queryParams.toString());
+	let r = quotes[intId % quotes.length];
+	q = r["quote"];
+	if(r["author"] != ""){
+		a = r["author"];
+	} else {
+		a = "unknown";
+	}
+	var queryParams = new URLSearchParams(window.location.search);
+	queryParams.set("id", intId);
+	history.replaceState(null, null, "?"+queryParams.toString());
 
-  id = intId;
+	id = intId;
 }
 
-function showQuote() {
+function showQuote(){
   ready = false;
   queryQuote();
 
@@ -50,22 +50,22 @@ function showQuote() {
 }
 
 $("body").click(() => {
-  if (ready) {
-    id = -1;
-    showQuote();
-  }
+	if(ready){
+		id = -1;
+		showQuote();
+	}
 });
 
-$(document).keydown(function (e) {
-  if (ready) {
-    if (e.which == 37) {
-      id--;
-      showQuote();
-    } else if (e.which == 39) {
-      id++;
-      showQuote();
-    }
-  }
+$(document).keydown(function(e){
+		if(ready){
+	    if (e.which == 37) {
+	       id--;
+				 showQuote();
+	    } else if (e.which == 39) {
+	       id++;
+				 showQuote();
+	    }
+		}
 });
 
 let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
@@ -73,7 +73,7 @@ let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 let bubs = [];
 let bg;
 
-function preload() {
+function preload(){
   bg = loadImage("bg.jpg");
 }
 
@@ -81,59 +81,59 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(bg);
   showQuote();
-  if (!isMobile) for (let i = 0; i < 10; i++) bubs.push(new Bubble());
+  if(!isMobile) for(let i = 0; i < 10; i++) bubs.push(new Bubble());
 }
 
 function draw() {
-  if (!isMobile) {
+  if(!isMobile){
     background(bg);
     bubs.forEach((i) => i.draw());
   }
 }
 
-class Bubble {
-  constructor() {
-    this.speed = random(0.5, 1);
-    this.dia = random(50, 70);
+class Bubble{
+  constructor(){
+    this.speed = random(0.5,1);
+    this.dia = random(50,70);
     this.diaAdd = 0.1;
-    this.pos = createVector(random(this.dia / 2, width - this.dia / 2), random(this.dia / 2, height - this.dia / 2));
-    this.dir = createVector(random(-1, 1), random(-1, 1));
+    this.pos = createVector(random(this.dia/2,width-this.dia/2),random(this.dia/2,height-this.dia/2));
+    this.dir = createVector(random(-1,1),random(-1,1));
     this.dir.normalize();
   }
 
-  draw() {
-    stroke(0, 0, 0, 0.3 * 255);
-    fill(255, 255, 255, 0.3 * 255);
+  draw(){
+    stroke(0,0,0,0.2*255);
+    fill(255,255,255,0.2*255);
     circle(this.pos.x, this.pos.y, this.dia);
 
     this.pos.add(p5.Vector.mult(this.dir, this.speed));
 
-    if (this.pos.x + this.dia / 2 >= width) {
+    if(this.pos.x + this.dia/2 >= width){
       this.dir.x *= -1;
-      this.pos.x = width - this.dia / 2;
+      this.pos.x = width - this.dia/2;
     }
 
-    if (this.pos.x - this.dia / 2 <= 0) {
+    if(this.pos.x - this.dia/2 <= 0){
       this.dir.x *= -1;
-      this.pos.x = this.dia / 2;
+      this.pos.x = this.dia/2;
     }
 
-    if (this.pos.y + this.dia / 2 >= height) {
+    if(this.pos.y + this.dia/2 >= height){
       this.dir.y *= -1;
-      this.pos.y = height - this.dia / 2;
+      this.pos.y = height - this.dia/2;
     }
 
-    if (this.pos.y - this.dia / 2 <= 0) {
+    if(this.pos.y - this.dia/2 <= 0){
       this.dir.y *= -1;
-      this.pos.y = this.dia / 2;
+      this.pos.y = this.dia/2;
     }
 
     this.dia += this.diaAdd;
-    if (this.dia >= 70) {
+    if(this.dia >= 70){
       this.diaAdd = -this.diaAdd;
       this.dia = 70;
     }
-    if (this.dia <= 50) {
+    if(this.dia <= 50){
       this.diaAdd = -this.diaAdd;
       this.dia = 50;
     }
